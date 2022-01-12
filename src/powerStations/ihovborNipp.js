da4ff619b4fa264d1cbc067404cb552a422cb42a
princeak <akalodave@gmail.com>
2022-01-12 07:46:10 +0100

    latest changes in ihovbor

--- src/powerStations/ihovborNipp.js
+++ src/powerStations/ihovborNipp.js
@@ -6,31 +6,37 @@ export var ihovborNipp = {
     units: [
         {
             name: "gt1",
+            station: "Ihovbor NIPP",
             id: "gt1",
             powerData
         },
         {
             name: "gt2",
+            station: "Ihovbor NIPP",
             id: "gt2",
             powerData
         },
         {
             name: "gt3",
+            station: "Ihovbor NIPP",
             id: "gt3",
             powerData
         },
         {
             name: "gt4",
+            station: "Ihovbor NIPP",
             id: "gt4",
             powerData
         },
         {
             name: "OHL1",
+            station: "AZURRA POWER",
             id: "ohl1",
             powerData
         },
         {
             name: "OHL2",
+            station: "AZURRA POWER",
             id: "ohl2",
             powerData
         }
