1a5577befb6d00ceddfff96ba40db6dbe6ab1fda
NCC IOT <ncc@gmail.com>
2022-10-27 13:42:24 +0100

    latest changes

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const SOCKET_ADDR = "ws://193.148.63.148:8080/";
+export const SOCKET_ADDR = "ws://172.16.200.4:8080/";
 export const STORAGE_KEY = "tcn-accessToken";
 export const LOGIN_ADDR = "http://193.148.63.148:3001/login";
